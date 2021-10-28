6db38ab0e6cb8ce629a5b30690b72e55f22a9f93
Leah <leahpitter95@gmail.com>
2021-10-28 19:25:27 +0100

    all tests green for statement class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,19 +4,20 @@ describe Statement do
 
   let(:statement) { Statement.new } 
   let(:transactions) { [transaction1, transaction2] }
-  let(:transaction1) { double(:transaction1, debit: "0.00", credit: "30.00",\
+  let(:transaction1) { double(:transaction1, credit: "30.00", debit: "0.00",\
                         date: "28-10-21", balance: "30.00")
   }
-  let(:transaction2) { double(:transaction2, debit: "10.00", credit: "0.00",\
+  let(:transaction2) { double(:transaction2, credit: "0.00", debit: "10.00",\
                         date: "29-10-21", balance: "20.00")
   }
 
   describe '#print' do
     it 'prints an array in the set format' do
-      text = 'date || credit || debit || balance'\
-      '15-09-21 || 10.00 || 0.00 || 10.00'\
-      '16-09-21 || 100.00 || 0.00 || 110.00'
-    expect {statement.print(transactions) }.to output(text).to_stdout
+      text = "date || credit || debit || balance\n"\
+      "29-10-21 || 0.00 || 10.00 || 20.00\n"\
+      "28-10-21 || 30.00 || 0.00 || 30.00\n"
+      
+    expect { statement.print(transactions) }.to output(text).to_stdout
     end
   end
 end
